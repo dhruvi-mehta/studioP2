060024ea45347067a082bbbbbf1fc684a38d5335
dhruvi-mehta <mehtd083@newschool.edu>
2021-03-01 02:05:31 -0500

    hi

--- script.js
+++ script.js
@@ -10,7 +10,3 @@ $(document).ready(function() {
 });
 
 
-var spin = ["spin1", "spin2", "spin3", "spin4", "spin5", "spin6", "spin7","spin8","spin9","spin10","spin11","spin12"];
-
-var randomItem = spin[Math.floor(Math.random()*spin.length)];
-console.log(random, spin[random]);
